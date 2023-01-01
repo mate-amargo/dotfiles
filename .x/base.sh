0803ef67642fd3617c8dca03d1a2f3313f5f9e36
Juan Alberto Regalado Galvan <00jarg@gmail.com>
2023-01-01 13:51:40 -0300

    Add ralt

--- .x/base.sh
+++ .x/base.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-setxkbmap us,latam -option grp:alt_space_toggle -option caps:swapescape
+setxkbmap us,latam -option grp:alt_space_toggle -option caps:swapescape -option compose:ralt
 multimonitor
