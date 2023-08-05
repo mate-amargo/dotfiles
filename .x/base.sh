add50521bcab822172eec19ae48f0a1b90066a0c
Juan Alberto Regalado Galvan <00jarg@gmail.com>
2023-08-05 14:10:54 -0300

    refactor(keyboard): Add ru layout, and replace latam with us with altgr-intl variant

--- .x/base.sh
+++ .x/base.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-setxkbmap us,latam -option grp:alt_space_toggle -option caps:swapescape -option compose:ralt
+setxkbmap -layout "us,ru" -variant "altgr-intl," -option nodeadkeys -option caps:swapescape -option grp:alt_space_toggle
+
 multimonitor
