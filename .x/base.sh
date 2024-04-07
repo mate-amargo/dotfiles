5b36f2bbaf1189218cececebbae53609279b03f9
Juan Alberto Regalado Galvan <00jarg@gmail.com>
2024-04-06 23:26:51 -0300

    fix(xinit): Call system xinit to setup dbus things before

--- .x/base.sh
+++ .x/base.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+ for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+  [ -x "$f" ] && . "$f"
+ done
+ unset f
+fi
 setxkbmap -layout "us,ru" -variant "altgr-intl," -option nodeadkeys -option caps:swapescape -option grp:alt_space_toggle
 
 multimonitor
