e03a8047da4fcfd11faf9cab29866788b6bfed40
Juan Alberto Regalado Galvan <00jarg@gmail.com>
2024-09-18 21:11:56 -0300

    feat(home): Add autorotate script for touchscreen

--- .x/base.sh
+++ .x/base.sh
@@ -6,5 +6,5 @@ if [ -d /etc/X11/xinit/xinitrc.d ] ; then
  unset f
 fi
 setxkbmap -layout "us,ru" -variant "altgr-intl," -option nodeadkeys -option caps:swapescape -option grp:alt_space_toggle
-
 multimonitor
+autorotate &
