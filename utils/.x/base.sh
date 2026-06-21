144e5841d7337802897eb7fb6307087d067f89a7
Juan Alberto Regalado Galvan <00jarg@gmail.com>
2026-06-21 20:29:21 -0300

    fix: launch wallchange to have a wallpaper on start

--- utils/.x/base.sh
+++ utils/.x/base.sh
@@ -7,4 +7,5 @@ if [ -d /etc/X11/xinit/xinitrc.d ] ; then
 fi
 setxkbmap -layout "us,ru" -variant "altgr-intl," -option nodeadkeys -option caps:swapescape -option grp:alt_space_toggle
 multimonitor
+wallchange
 autorotate &
